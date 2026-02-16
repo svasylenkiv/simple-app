b14a54c584d3eaf03b61d4e0ba273db76a24a908
Stepan Vasylenkiv <svasylenkiv@gmail.com>
2026-02-16 19:44:34 +0200

    Add sudo prefix

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-dnf update -y
-dnf install -y docker
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ec2-user
+sudo dnf update -y
+sudo dnf install -y docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
